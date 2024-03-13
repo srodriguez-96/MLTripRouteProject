c8e64c4db006c73582e72e930fb37ee96442bae3
tonyr <tonyromero394@gmail.com>
2024-03-12 22:48:01 -0600

    changes to README and Node class added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MLTripRouteProject
+
+## Node.py
+    node.py will hold the basic node information that will be gained from overpass turbos JSON strings.
---
+++ node.py
@@ -0,0 +1,8 @@
+class locationNode:
+    def __init__(self, name, lat, lon, amenity):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+        self.amenity = amenity
+
+    

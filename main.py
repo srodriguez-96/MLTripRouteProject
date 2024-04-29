60a1493e6314f49b5936001de72d0f2c4fadb668
tonyr <tonyromero394@gmail.com>
2024-04-29 02:28:16 -0600

    created basic layout for how data will be extracted

--- main.py
+++ main.py
@@ -20,10 +20,30 @@ def main():
     # Remove specified columns
     df = df.drop(columns=columns_to_remove, axis=1)
 
+    feature1 = 'BusinessAcceptsCreditCards'
+    feature2 = 'WiFi'
+    feature3 = 'RestaurantsPriceRange2'
+    dropRows = []
+    feature1List = []
+    feature2List = []
+    feature3List = []
+
     for index, row in df.iterrows():
         value = row.iloc[2]
-        print(value)
-        print(type(value))
+        stringDict = eval(value)
+        
+        if feature1 & feature2 & feature3 in stringDict:
+            #if feature are in this row we add to the list to append to the df later
+            #these lists will be the new features that are extracted
+            value1 = stringDict.get(feature1)
+            feature1List.append(value1)
+            value2 = stringDict.get(feature2)
+            feature2List.append(value2)
+            value3 = stringDict.get(feature3)
+            feature3List.append(value3)
+        else:
+            #if feature not in this row we are going to drop this row later
+            dropRows.append(index)
         break
 
     sc = StandardScaler()
